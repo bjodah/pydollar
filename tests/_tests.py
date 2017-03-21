988bcb94b6f2460b96e262a11c3735fbcc1809c5
Björn Dahlgren <bjodah@gmail.com>
2017-03-21 18:33:39 +0100

    Test renumbering

--- tests/_tests.py
+++ tests/_tests.py
@@ -1,27 +1,27 @@
-
 # here's a dollar sign in a comment $
+from collections import OrderedDict
 
 my_evil_values = ('_DOLLAR_SIGN_LITERAL_0_', '_DOLLAR_SIGN_LITERAL_1_')
 price = '$42'
 
-def case_1():
+def case_0():
     a, b, c = args = $
     return args
-case_1.expected = ('a', 'b', 'c')
+case_0.expected = ('a', 'b', 'c')
 
-def case_2():
+def case_1():
     hello, world = map(str.capitalize, $)
     return hello, world
-case_2.expected = ('Hello', 'World')
+case_1.expected = ('Hello', 'World')
 
-def case_3():
+def case_2():
     houston, do, you, copy = args = [word.upper() for word in $]
     return ' '.join(args)
-case_3.expected = 'HOUSTON DO YOU COPY'
+case_2.expected = 'HOUSTON DO YOU COPY'
 
-def case_4():
-    alpha, beta, gamma = d = {k: k.upper() for k in $}
+def case_3():
+    alpha, beta, gamma = d = OrderedDict([(k, k.upper()) for k in $])
     return d
-case_4.expected = {'alpha': 'ALPHA', 'beta': 'BETA', 'gamma': 'GAMMA'}
+case_3.expected = {'alpha': 'ALPHA', 'beta': 'BETA', 'gamma': 'GAMMA'}
 
-cases = (case_1, case_2, case_3, case_4)
+cases = (case_0, case_1, case_2, case_3)
